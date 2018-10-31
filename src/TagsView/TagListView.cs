ab2b753180ef748d76fbb6fe7126c4cc50f14249
ALI USMAN <ali.usman.net@gmail.com>
2018-10-31 11:31:42 +0930

    Update TagListView.cs

--- src/TagsView/TagListView.cs
+++ src/TagsView/TagListView.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Cirrious.FluentLayouts.Touch; // This is required
+using Cirrious.FluentLayouts.Touch;
 using UIKit;
 
 namespace TagsView
